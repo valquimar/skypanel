f3498ed399e962c9ef11654bf4c88bdfe2356e1b
valquimar <valquimar@hotmail.com>
2025-08-12 17:34:03 -0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h2>Bem-vindo ao Skypanel</h2>
+      <p>Seu painel inteligente está pronto para decolar 🚀</p>
+    </main>
+  )
+}
+
+export default Home
