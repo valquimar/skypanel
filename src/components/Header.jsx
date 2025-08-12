1d1d980ce3ca8aee6176369ef1a5aad08096dc31
valquimar <valquimar@hotmail.com>
2025-08-12 17:32:39 -0300

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header style={{ padding: '1rem', background: '#222', color: '#fff' }}>
+      <h1>Skypanel</h1>
+    </header>
+  )
+}
+
+export default Header
